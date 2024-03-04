483be48c8f9903ae9ef55bd1e0272444597dd531
wxf <991915261@qq.com>
2024-03-04 14:12:07 +0800

    add 'full screen code'

--- alien_invasion.py
+++ alien_invasion.py
@@ -11,7 +11,10 @@ class AlienInvasion:
         """创建游戏资源"""
         pygame.init()
         self.clock = pygame.time.Clock()
-        self.settings = Settings()
+        self.settings: Settings = Settings()
+        # self.screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
+        # self.settings.screen_width = self.screen.get_rect().width
+        # self.settings.screen_height = self.screen.get_rect().height
         self.screen = pygame.display.set_mode((self.settings.screen_width,
                                               self.settings.screen_height))
         pygame.display.set_caption("Alien Invasion")
@@ -39,7 +42,9 @@ class AlienInvasion:
             self.ship.moving_right = True
         elif event.key == pygame.K_LEFT:
             self.ship.moving_left = True
-            
+        elif event.key == pygame.K_q:
+
+            sys.exit()
 
     def _check_keyup_events(self, event: pygame.event):
         if event.key == pygame.K_RIGHT:
