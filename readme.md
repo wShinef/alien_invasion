30d71211ca61d5abea97dec935d8a38b271193df
wxf <991915261@qq.com>
2024-03-05 18:35:23 +0800

    调整忽略文件

---
+++ readme.md
@@ -0,0 +1,4 @@
+python编程:从入门到实践（第3版）
+外星人入侵项目，纯手工实现
+Python 3.7.6
+<div>pip install -r requirements.txt</div>
---
+++ ship.py
@@ -0,0 +1,29 @@
+import pygame
+from settings import Settings
+
+
+class Ship:
+    def __init__(self, ai_game):
+        self.screen = ai_game.screen
+        self.screen_rect = ai_game.screen.get_rect()
+        self.image = pygame.image.load('images/ship.bmp')
+        self.settings: Settings = ai_game.settings
+        self.rect = self.image.get_rect()
+        self.rect.midbottom = self.screen_rect.midbottom
+        self.x = float(self.rect.x)
+        self.moving_right = False
+        self.moving_left = False
+
+    def blitme(self):
+        self.screen.blit(self.image, self.rect)
+
+    def update(self):
+        if self.moving_right and self.rect.x < self.screen_rect.right - (self.rect.right - self.rect.left):
+            self.x += self.settings.ship_speed
+        if self.moving_left and self.rect.x > 0:
+            self.x -= self.settings.ship_speed
+        self.rect.x = self.x
+
+    def center_ship(self):
+        self.rect.midbottom = self.screen_rect.midbottom
+        self.x = float(self.rect.x)
